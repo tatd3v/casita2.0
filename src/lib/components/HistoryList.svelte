dbc8f649486f0f27d0ff146f48926dcdcdfe67dd
tatd3v <tatdev91@gmail.com>
2026-01-08 15:50:09 -0500

    Add stopPropagation to toggle icon click handler and fix missing closing if tag
    - Add stopPropagation to toggle-icon click handler to prevent event bubbling to day-header
    - Add missing {/if} closing tag after day group content div

--- src/lib/components/HistoryList.svelte
+++ src/lib/components/HistoryList.svelte
@@ -172,7 +172,7 @@
             <div class="day-header" class:clickable={isCollapsible(dateString)} on:click={() => isCollapsible(dateString) && toggleDate(dateString)}>
               <h4>{new Date(dateString).toLocaleDateString(localeOptions[locale], { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' })}</h4>
               {#if isCollapsible(dateString)}
-                <div class="toggle-icon" class:collapsed={collapsedDates.has(dateString)}>
+                <div class="toggle-icon" class:collapsed={collapsedDates.has(dateString)} on:click|stopPropagation={() => toggleDate(dateString)}>
                   <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                   </svg>
@@ -195,6 +195,7 @@
               </ul>
             {/if}
           </div>
+        {/if}
       {/each}
     </div>
   {:else}
